03f508f2db234b39ca406f69ff9ca0eac06135d4
jcarrasco <jafet.carrasco@tecsup.edu.pe>
2020-10-08 22:09:04 -0500

    Second commit - Jcarrasco

--- src/main/java/com/tecsup/lab03/controller/App.java
+++ src/main/java/com/tecsup/lab03/controller/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	//------------
+    	// New Branch
+    	//------------
+    	
         System.out.println( "Hello World!" );
+        
+        System.out.println( "Nuevo Branch!" );
     }
 }
